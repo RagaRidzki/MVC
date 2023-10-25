c5e12c674128c024b84bc208a8f1269cb74404b2
Raga Ridzki Panuntun <115514988+RagaRidzki@users.noreply.github.com>
2023-10-25 20:10:28 +0700

    Add files via upload

---
+++ PHPMVC/app/views/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h1 class="display-4">Selamat Datang</h1>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi a veritatis nihil earum aut fugit esse voluptate. Optio, nemo molestiae!</p>
+    </div>
+</div>
